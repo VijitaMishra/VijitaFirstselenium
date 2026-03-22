68e5f868da2948b8ba6521da7da31b29456e2915
VijitaMishra <vijita.zebra@gmail.com>
2026-03-22 09:56:03 -0400

    faker

--- VijitaFirstselenium/src/main/java/faker/Tryfaker.java
+++ VijitaFirstselenium/src/main/java/faker/Tryfaker.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.github.javafaker.Address;
+import com.github.javafaker.Faker;
+
 public class Tryfaker {
 	
 	public static void main (String[] args) throws InterruptedException {
@@ -20,33 +23,34 @@ public class Tryfaker {
 		WebElement registerele=driver.findElement(By.xpath("//a[text()='Register your account']"));
 		registerele.click();
 		WebElement firstnameele=driver.findElement(By.xpath("//input[@id='first_name']"));
-		firstnameele.sendKeys("Tom");
+		firstnameele.sendKeys(new Faker().name().firstName());
 		WebElement lastnameele=driver.findElement(By.xpath("//input[@id='last_name']"));
-		lastnameele.sendKeys("Smith");
+		lastnameele.sendKeys(new Faker().name().lastName());
 		WebElement dobele=driver.findElement(By.xpath("//input[@id='dob']"));
 		dobele.sendKeys("1990-08-25");
 		WebElement addressele=driver.findElement(By.xpath("//input[@id='address']"));
-		addressele.sendKeys("2 INDERHEIGHTS DR");
+		addressele.sendKeys(new Faker().address().fullAddress());
 		WebElement postalcodeele=driver.findElement(By.xpath("//input[@id='postcode']"));
-		postalcodeele.sendKeys("L6Z 3M4");
+		postalcodeele.sendKeys(new Faker().address().zipCode());
 		WebElement cityele=driver.findElement(By.xpath("//input[@id='city']"));
-		cityele.sendKeys("Brampton");
+		cityele.sendKeys( new Faker().address().city());
 		WebElement countryele=driver.findElement(By.xpath("//input[@id='state']"));
-		countryele.sendKeys("Ontario");
+		countryele.sendKeys(new Faker().address().state());
 		WebElement stateele=driver.findElement(By.xpath("//select[@id='country']"));
-		stateele.sendKeys("Canada");
+		stateele.sendKeys(new Faker().address().country());
 		WebElement phoneele=driver.findElement(By.xpath("//input[@id='phone']"));
-		phoneele.sendKeys("4253658524");
+		phoneele.sendKeys(new Faker().phoneNumber().subscriberNumber());
 		WebElement emailele=driver.findElement(By.xpath("//input[@id='email']"));
-		emailele.sendKeys("tom.smith@hotmail.com");
+		emailele.sendKeys(new Faker().internet().emailAddress());
 		WebElement passwordele=driver.findElement(By.xpath("//input[@id='password']"));
-		passwordele.sendKeys("Tomsmi@1235");
+		passwordele.sendKeys(new Faker().internet().password());
 		
 		Thread.sleep(2000);
-		WebElement submitele=driver.findElement(By.xpath("//div/button[@data-test='register-submit']"));
+		WebElement submitele=driver.findElement(By.xpath("//button[@data-test='register-submit']"));
 		submitele.click();
 		
 	}
+	
 
 }
 
