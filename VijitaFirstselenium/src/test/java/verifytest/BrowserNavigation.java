71ed320e5abeca4e959a6ed4f1a424bb03671027
VijitaMishra <vijita.zebra@gmail.com>
2026-04-02 12:23:19 -0400

    browse

--- VijitaFirstselenium/src/test/java/verifytest/BrowserNavigation.java
+++ VijitaFirstselenium/src/test/java/verifytest/BrowserNavigation.java
@@ -1,5 +1,7 @@
 package verifytest;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,6 +15,7 @@ public class BrowserNavigation {
 	public void testbrowsernavi() {
 		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://the-internet.herokuapp.com/");
 		
 		WebElement formauthele = driver.findElement(By.xpath("//a[text()='Form Authentication']"));
@@ -22,5 +25,14 @@ public class BrowserNavigation {
 		String head = headingele.getText();
 		
 		Assert.assertEquals(head, "Login Page");
+		
+		driver.navigate().back();
+		
+		WebElement homepageele = driver.findElement(By.xpath("//h1[text()='Welcome to the-internet']"));
+		head = homepageele.getText();
+		
+		Assert.assertEquals(head, "Welcome to the-internet");
+		
+		driver.quit();
 	}
 }
